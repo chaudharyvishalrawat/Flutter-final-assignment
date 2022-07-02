0bdccfb632b2ac20800b33c846207be388b55a26
chaudharyvishalrawat <104204831+chaudharyvishalrawat@users.noreply.github.com>
2022-07-02 16:07:29 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:challenge/challenge.dart';
+import 'package:challenge/login.dart';
+import 'package:challenge/screen3.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: challenge(),
+  ));
+}
